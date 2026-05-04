2cafd7196009d28d50a223d736b6e864daa95df6
pablgonzalezr-cyber <pabl.gonzalezr@duocuc.cl>
2026-05-04 13:24:09 -0400

    configura api gateway

--- api-gateway/api-gateway/src/main/java/GameShelf/api_gateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/GameShelf/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package GameShelf.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // configuramos gateway para conectar los microservicios entre si
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
