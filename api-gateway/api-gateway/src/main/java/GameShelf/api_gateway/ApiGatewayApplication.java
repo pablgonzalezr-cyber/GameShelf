59e21ee8e5c004ad1edc3163118fc6dc538d6498
pablgonzalezr-cyber <pabl.gonzalezr@duocuc.cl>
2026-05-04 13:27:22 -0400

    agregamos comentarios en eureka y apigateway

--- api-gateway/api-gateway/src/main/java/GameShelf/api_gateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/GameShelf/api_gateway/ApiGatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient // configuramos gateway para conectar los microservicios entre si
+@EnableDiscoveryClient // configuramos gateway para conectar los microservicios entre sia traves de eureka
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- eureka-Server/eureka-Server/src/main/java/GameShelf/eureka_Server/EurekaServerApplication.java
+++ eureka-Server/eureka-Server/src/main/java/GameShelf/eureka_Server/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication  
-@EnableEurekaServer     // Conectamos eurekaServer
+@EnableEurekaServer     // Activa este proyecto como servidor Eureka para registrar los microservicios
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
