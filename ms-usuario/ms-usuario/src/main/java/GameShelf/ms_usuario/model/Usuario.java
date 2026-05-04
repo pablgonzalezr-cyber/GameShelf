f6a110810e4933f75b3d95f787b1464d392dc3f8
Gabriel Chodil <ga.chodil@duocuc.cl>
2026-05-04 10:34:50 -0400

    Inicio programacion ms-usuario

---
+++ ms-usuario/ms-usuario/src/main/java/GameShelf/ms_usuario/model/Usuario.java
@@ -0,0 +1,34 @@
+package GameShelf.ms_usuario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String usuario;
+
+    @Column(nullable = false)
+    private String contraseña;
+
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    private String rol;
+}
---
+++ ms-usuario/ms-usuario/src/main/java/GameShelf/ms_usuario/repository/UsuarioRepository.java
@@ -0,0 +1,42 @@
+package GameShelf.ms_usuario.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import GameShelf.ms_usuario.model.Usuario;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    // Buscar por email (Fundamental para login o recuperación de contraseña)
+    Optional<Usuario> findByCorreo (String correo);
+
+    // Buscar por nombre de usuario (Para login)
+    Optional<Usuario> findByUsuario (String usuario);
+
+    // 2. VALIDACIONES DE EXISTENCIA (Para el proceso de Registro/Sign Up)
+    // Estos devuelven true o false. Es más rápido que traer todo el objeto.
+    
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
+
+    // 3. BÚSQUEDAS DE LISTAS (Para administración)
+    
+    // Buscar todos los usuarios que tienen un rol específico (ej: todos los "ADMIN")
+    List<Usuario> findByRol(String rol);
+
+    // Buscar usuarios cuyo nombre contenga una palabra (para buscadores con lupa)
+    List<Usuario> findByUsernameContainingIgnoreCase(String username);
+
+
+    // 4. BORRADO SEGURO
+    
+    // Borrar por email en lugar de solo por ID
+    void deleteByEmail(String email);
+
+}
